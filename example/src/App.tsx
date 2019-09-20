e06f46de2d955b841d41ee175f1895e6a3840f92
Illia Kibalnyi <sheyn.kiba@gmail.com>
2019-09-20 16:14:39 +0200

    Add example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
-// import { useFetch } from '@kibatus/react-use-fetch'
+import { useFetch } from '@kibatus/react-use-fetch'
+import './App.css';
+
+const  fetchPoll  =  (forceError?: string) => fetch('https://api.quotable.io/random')
+  .then((response) => {
+    if (forceError || !response.ok) {
+      return Promise.reject(forceError || response.statusText);
+    }
+    return response;
+  })
+  .then(response => response.json())
+  .then(data => data);
 
 const App = () => {
-  // const example = useFetch();
+  const  { data, isLoading, error, refetch } = useFetch(fetchPoll);
+
+  console.log(error);
+
   return (
-    <div>
-      <h1>Demo useFetch</h1>
+    <div className="App-wrapper box">
+      <div className="App-message">
+        {isLoading ? 'Loading...' : (
+          error || (data && data.content)
+        )}
+      </div>
+      <div className="App-actions-wrapper">
+        <button className="btn btn-primary" onClick={() => refetch()}>Get new quote</button>
+        <button className="btn btn-secondary" onClick={() => refetch('Manually generated error')}>Fetch with error</button>
+      </div>
     </div>
   )
 };
